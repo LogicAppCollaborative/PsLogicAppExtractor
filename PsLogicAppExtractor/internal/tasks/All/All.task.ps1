7c98a891f8b99012a90ff03c26ae532d97c4924e
Mötz Jensen <motz@e-s.dk>
2022-12-09 13:40:40 +0100

    Feature: Add support for pure recurrence triggers also
    Handle StartTime and TimeZone properties

--- PsLogicAppExtractor/internal/tasks/All/All.task.ps1
+++ PsLogicAppExtractor/internal/tasks/All/All.task.ps1
@@ -2383,9 +2383,12 @@ Task -Name "Set-Arm.Tags.AsVariable" @parm -Action {
 #Original file: Set-Arm.Trigger.ApiConnection.EvaluatedRecurrence.AsParameter.task.ps1
 $parm = @{
     Description = @"
-Creates the evaluatedRecurrence property
--Sets the recurrence.frequency value to: [parameters('trigger_Frequency')]
--Sets the recurrence.interval value to: [parameters('trigger_Interval')]
+    Creates an Arm parameter: trigger_EvalFrequency
+    -Sets the default value to the original value, extracted from evaluatedRecurrence.frequency
+    -Sets the evaluatedRecurrence.frequency value to: [parameters('trigger_EvalFrequency')]
+    Creates an Arm parameter: trigger_EvalInterval
+    -Set the default value to the original value, extracted from evaluatedRecurrence.interval
+    -Sets the evaluatedRecurrence.interval value to: [parameters('trigger_EvalInterval')]
 "@
     Alias       = "Arm.Set-Arm.Trigger.ApiConnection.EvaluatedRecurrence.AsParameter"
 }
@@ -2395,21 +2398,61 @@ Task -Name "Set-Arm.Trigger.ApiConnection.EvaluatedRecurrence.AsParameter" @parm
 
     $armObj = Get-TaskWorkObject
 
-    if ($armObj.resources[0].properties.definition.triggers.PsObject.Properties.Value.type -eq "ApiConnection" -and
+    if ($armObj.resources[0].properties.definition.triggers.PsObject.Properties.Value.type -in @("ApiConnection", "Recurrence") -and
         $armObj.resources[0].properties.definition.triggers.PsObject.Properties.Value.recurrence) {
 
-        $frequencyPreSuf = Format-Name -Type "Trigger" -Prefix $Trigger_Prefix -Suffix $Trigger_Suffix -Value "Frequency"
-        $intervalPreSuf = Format-Name -Type "Trigger" -Prefix $Trigger_Prefix -Suffix $Trigger_Suffix -Value "Interval"
+        $frequencyPreSuf = Format-Name -Type "Trigger" -Prefix $Trigger_Prefix -Suffix $Trigger_Suffix -Value "EvalFrequency"
+        $intervalPreSuf = Format-Name -Type "Trigger" -Prefix $Trigger_Prefix -Suffix $Trigger_Suffix -Value "EvalInterval"
+        $startTimePreSuf = Format-Name -Type "Trigger" -Prefix $Trigger_Prefix -Suffix $Trigger_Suffix -Value "EvalStartTime"
+        $timeZonePreSuf = Format-Name -Type "Trigger" -Prefix $Trigger_Prefix -Suffix $Trigger_Suffix -Value "EvalTimeZone"
 
         if ($null -eq @($armObj.resources[0].properties.definition.triggers.PsObject.Properties)[0].Value.evaluatedRecurrence) {
+            # Create the evaluatedRecurrence property if it does not exist
             @($armObj.resources[0].properties.definition.triggers.PsObject.Properties)[0].Value | Add-Member -MemberType NoteProperty -Name "evaluatedRecurrence" -Value $([ordered]@{
                     frequency = "Minute";
                     interval  = 1;
                 })
         }
 
-        @($armObj.resources[0].properties.definition.triggers.PsObject.Properties)[0].Value.evaluatedRecurrence.frequency = "[parameters('$frequencyPreSuf')]"
-        @($armObj.resources[0].properties.definition.triggers.PsObject.Properties)[0].Value.evaluatedRecurrence.interval = "[parameters('$intervalPreSuf')]"
+        # Handle the frequency property
+        $orgFrequency = $armObj.resources[0].properties.definition.triggers.PsObject.Properties.Value.evaluatedRecurrence.frequency
+        $armObj = Add-ArmParameter -InputObject $armObj -Name "$frequencyPreSuf" `
+            -Type "string" `
+            -Value $orgFrequency `
+            -Description "The frequency used for the trigger to evalutate."
+
+        $armObj.resources[0].properties.definition.triggers.PsObject.Properties.Value.evaluatedRecurrence.frequency = "[parameters('$frequencyPreSuf')]"
+        
+        # Handle the interval property
+        $orgInterval = $armObj.resources[0].properties.definition.triggers.PsObject.Properties.Value.evaluatedRecurrence.interval
+        $armObj = Add-ArmParameter -InputObject $armObj -Name "$intervalPreSuf" `
+            -Type "int" `
+            -Value $orgInterval `
+            -Description "The interval used for the trigger to evalutate."
+
+        $armObj.resources[0].properties.definition.triggers.PsObject.Properties.Value.evaluatedRecurrence.interval = "[parameters('$intervalPreSuf')]"
+
+        # Handle the StartTime property
+        $orgStartTime = $armObj.resources[0].properties.definition.triggers.PsObject.Properties.Value.evaluatedRecurrence.startTime
+        if (-not ($null -eq $orgStartTime)) {
+            $armObj = Add-ArmParameter -InputObject $armObj -Name "$startTimePreSuf" `
+                -Type "string" `
+                -Value $orgStartTime `
+                -Description "The start time used for the trigger to evalutate."
+
+            $armObj.resources[0].properties.definition.triggers.PsObject.Properties.Value.evaluatedRecurrence.startTime = "[parameters('$startTimePreSuf')]"
+        }
+
+        # Handle the TimeZone property
+        $orgTimeZone = $armObj.resources[0].properties.definition.triggers.PsObject.Properties.Value.evaluatedRecurrence.timeZone
+        if (-not ($null -eq $orgTimeZone)) {
+            $armObj = Add-ArmParameter -InputObject $armObj -Name "$timeZonePreSuf" `
+                -Type "string" `
+                -Value $orgTimeZone `
+                -Description "The time zone used for the trigger to evalutate."
+
+            $armObj.resources[0].properties.definition.triggers.PsObject.Properties.Value.evaluatedRecurrence.timeZone = "[parameters('$timeZonePreSuf')]"
+        }
     }
 
     Out-TaskFileArm -InputObject $armObj
@@ -2433,27 +2476,53 @@ Task -Name "Set-Arm.Trigger.ApiConnection.Recurrence.AsParameter" @parm -Action 
 
     $armObj = Get-TaskWorkObject
 
-    if ($armObj.resources[0].properties.definition.triggers.PsObject.Properties.Value.type -eq "ApiConnection" -and
+    if ($armObj.resources[0].properties.definition.triggers.PsObject.Properties.Value.type -in @("ApiConnection", "Recurrence") -and
         $armObj.resources[0].properties.definition.triggers.PsObject.Properties.Value.recurrence) {
 
         $frequencyPreSuf = Format-Name -Type "Trigger" -Prefix $Trigger_Prefix -Suffix $Trigger_Suffix -Value "Frequency"
         $intervalPreSuf = Format-Name -Type "Trigger" -Prefix $Trigger_Prefix -Suffix $Trigger_Suffix -Value "Interval"
+        $startTimePreSuf = Format-Name -Type "Trigger" -Prefix $Trigger_Prefix -Suffix $Trigger_Suffix -Value "StartTime"
+        $timeZonePreSuf = Format-Name -Type "Trigger" -Prefix $Trigger_Prefix -Suffix $Trigger_Suffix -Value "TimeZone"
 
+        # Handle the Frequency property
         $orgFrequency = $armObj.resources[0].properties.definition.triggers.PsObject.Properties.Value.recurrence.frequency
         $armObj = Add-ArmParameter -InputObject $armObj -Name "$frequencyPreSuf" `
             -Type "string" `
             -Value $orgFrequency `
-            -Description "The frequency used for the trigger to evalutate / run."
+            -Description "The frequency used for the trigger to run."
 
         $armObj.resources[0].properties.definition.triggers.PsObject.Properties.Value.recurrence.frequency = "[parameters('$frequencyPreSuf')]"
         
+        # Handle the Interval property
         $orgInterval = $armObj.resources[0].properties.definition.triggers.PsObject.Properties.Value.recurrence.interval
         $armObj = Add-ArmParameter -InputObject $armObj -Name "$intervalPreSuf" `
             -Type "int" `
             -Value $orgInterval `
-            -Description "The interval used for the trigger to evalutate / run."
+            -Description "The interval used for the trigger to run."
 
         $armObj.resources[0].properties.definition.triggers.PsObject.Properties.Value.recurrence.interval = "[parameters('$intervalPreSuf')]"
+
+        # Handle the StartTime property
+        $orgStartTime = $armObj.resources[0].properties.definition.triggers.PsObject.Properties.Value.recurrence.startTime
+        if (-not ($null -eq $orgStartTime)) {
+            $armObj = Add-ArmParameter -InputObject $armObj -Name "$startTimePreSuf" `
+                -Type "string" `
+                -Value $orgStartTime `
+                -Description "The start time used for the trigger to evalutate."
+
+            $armObj.resources[0].properties.definition.triggers.PsObject.Properties.Value.recurrence.startTime = "[parameters('$startTimePreSuf')]"
+        }
+
+        # Handle the TimeZone property
+        $orgTimeZone = $armObj.resources[0].properties.definition.triggers.PsObject.Properties.Value.recurrence.timeZone
+        if (-not ($null -eq $orgTimeZone)) {
+            $armObj = Add-ArmParameter -InputObject $armObj -Name "$timeZonePreSuf" `
+                -Type "string" `
+                -Value $orgTimeZone `
+                -Description "The time zone used for the trigger to evalutate."
+
+            $armObj.resources[0].properties.definition.triggers.PsObject.Properties.Value.recurrence.timeZone = "[parameters('$timeZonePreSuf')]"
+        }
     }
 
     Out-TaskFileArm -InputObject $armObj
--- PsLogicAppExtractor/internal/tasks/Set-Arm.Trigger.ApiConnection.EvaluatedRecurrence.AsParameter.task.ps1
+++ PsLogicAppExtractor/internal/tasks/Set-Arm.Trigger.ApiConnection.EvaluatedRecurrence.AsParameter.task.ps1
@@ -1,8 +1,11 @@
 ﻿$parm = @{
     Description = @"
-Creates the evaluatedRecurrence property
--Sets the recurrence.frequency value to: [parameters('trigger_Frequency')]
--Sets the recurrence.interval value to: [parameters('trigger_Interval')]
+    Creates an Arm parameter: trigger_EvalFrequency
+    -Sets the default value to the original value, extracted from evaluatedRecurrence.frequency
+    -Sets the evaluatedRecurrence.frequency value to: [parameters('trigger_EvalFrequency')]
+    Creates an Arm parameter: trigger_EvalInterval
+    -Set the default value to the original value, extracted from evaluatedRecurrence.interval
+    -Sets the evaluatedRecurrence.interval value to: [parameters('trigger_EvalInterval')]
 "@
     Alias       = "Arm.Set-Arm.Trigger.ApiConnection.EvaluatedRecurrence.AsParameter"
 }
@@ -12,21 +15,61 @@ Task -Name "Set-Arm.Trigger.ApiConnection.EvaluatedRecurrence.AsParameter" @parm
 
     $armObj = Get-TaskWorkObject
 
-    if ($armObj.resources[0].properties.definition.triggers.PsObject.Properties.Value.type -eq "ApiConnection" -and
+    if ($armObj.resources[0].properties.definition.triggers.PsObject.Properties.Value.type -in @("ApiConnection", "Recurrence") -and
         $armObj.resources[0].properties.definition.triggers.PsObject.Properties.Value.recurrence) {
 
-        $frequencyPreSuf = Format-Name -Type "Trigger" -Prefix $Trigger_Prefix -Suffix $Trigger_Suffix -Value "Frequency"
-        $intervalPreSuf = Format-Name -Type "Trigger" -Prefix $Trigger_Prefix -Suffix $Trigger_Suffix -Value "Interval"
+        $frequencyPreSuf = Format-Name -Type "Trigger" -Prefix $Trigger_Prefix -Suffix $Trigger_Suffix -Value "EvalFrequency"
+        $intervalPreSuf = Format-Name -Type "Trigger" -Prefix $Trigger_Prefix -Suffix $Trigger_Suffix -Value "EvalInterval"
+        $startTimePreSuf = Format-Name -Type "Trigger" -Prefix $Trigger_Prefix -Suffix $Trigger_Suffix -Value "EvalStartTime"
+        $timeZonePreSuf = Format-Name -Type "Trigger" -Prefix $Trigger_Prefix -Suffix $Trigger_Suffix -Value "EvalTimeZone"
 
         if ($null -eq @($armObj.resources[0].properties.definition.triggers.PsObject.Properties)[0].Value.evaluatedRecurrence) {
+            # Create the evaluatedRecurrence property if it does not exist
             @($armObj.resources[0].properties.definition.triggers.PsObject.Properties)[0].Value | Add-Member -MemberType NoteProperty -Name "evaluatedRecurrence" -Value $([ordered]@{
                     frequency = "Minute";
                     interval  = 1;
                 })
         }
 
-        @($armObj.resources[0].properties.definition.triggers.PsObject.Properties)[0].Value.evaluatedRecurrence.frequency = "[parameters('$frequencyPreSuf')]"
-        @($armObj.resources[0].properties.definition.triggers.PsObject.Properties)[0].Value.evaluatedRecurrence.interval = "[parameters('$intervalPreSuf')]"
+        # Handle the frequency property
+        $orgFrequency = $armObj.resources[0].properties.definition.triggers.PsObject.Properties.Value.evaluatedRecurrence.frequency
+        $armObj = Add-ArmParameter -InputObject $armObj -Name "$frequencyPreSuf" `
+            -Type "string" `
+            -Value $orgFrequency `
+            -Description "The frequency used for the trigger to evalutate."
+
+        $armObj.resources[0].properties.definition.triggers.PsObject.Properties.Value.evaluatedRecurrence.frequency = "[parameters('$frequencyPreSuf')]"
+        
+        # Handle the interval property
+        $orgInterval = $armObj.resources[0].properties.definition.triggers.PsObject.Properties.Value.evaluatedRecurrence.interval
+        $armObj = Add-ArmParameter -InputObject $armObj -Name "$intervalPreSuf" `
+            -Type "int" `
+            -Value $orgInterval `
+            -Description "The interval used for the trigger to evalutate."
+
+        $armObj.resources[0].properties.definition.triggers.PsObject.Properties.Value.evaluatedRecurrence.interval = "[parameters('$intervalPreSuf')]"
+
+        # Handle the StartTime property
+        $orgStartTime = $armObj.resources[0].properties.definition.triggers.PsObject.Properties.Value.evaluatedRecurrence.startTime
+        if (-not ($null -eq $orgStartTime)) {
+            $armObj = Add-ArmParameter -InputObject $armObj -Name "$startTimePreSuf" `
+                -Type "string" `
+                -Value $orgStartTime `
+                -Description "The start time used for the trigger to evalutate."
+
+            $armObj.resources[0].properties.definition.triggers.PsObject.Properties.Value.evaluatedRecurrence.startTime = "[parameters('$startTimePreSuf')]"
+        }
+
+        # Handle the TimeZone property
+        $orgTimeZone = $armObj.resources[0].properties.definition.triggers.PsObject.Properties.Value.evaluatedRecurrence.timeZone
+        if (-not ($null -eq $orgTimeZone)) {
+            $armObj = Add-ArmParameter -InputObject $armObj -Name "$timeZonePreSuf" `
+                -Type "string" `
+                -Value $orgTimeZone `
+                -Description "The time zone used for the trigger to evalutate."
+
+            $armObj.resources[0].properties.definition.triggers.PsObject.Properties.Value.evaluatedRecurrence.timeZone = "[parameters('$timeZonePreSuf')]"
+        }
     }
 
     Out-TaskFileArm -InputObject $armObj
