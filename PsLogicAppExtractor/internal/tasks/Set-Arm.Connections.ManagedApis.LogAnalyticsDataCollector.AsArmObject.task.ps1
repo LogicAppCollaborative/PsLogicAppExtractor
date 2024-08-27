8e43b499ef1e514dca58fa3b1a04115db5499b06
Mötz Jensen <motz@e-s.dk>
2024-08-27 14:25:55 +0200

    Feature: New connector supported

--- PsLogicAppExtractor/internal/tasks/Set-Arm.Connections.ManagedApis.LogAnalyticsDataCollector.AsArmObject.task.ps1
+++ PsLogicAppExtractor/internal/tasks/Set-Arm.Connections.ManagedApis.LogAnalyticsDataCollector.AsArmObject.task.ps1
@@ -1,35 +1,32 @@
 ﻿$parm = @{
     Description = @"
 Loops all `$connections children
--Validates that is of the type servicebus, and that is using the classic connection string approach
+-Validates that is of the type AzureLogAnalyticsDataCollector
 --Creates a new resource in the ARM template, for the ApiConnection object
---With matching ARM Parameters, for the connection string as a Secure String
+--With matching ARM Parameters, for the 'InstrumentKey/Username' as a String
 --Makes sure the ARM Parameters logicAppLocation exists
---The type is based on ConnectionString approach
 --Displayname is extracted from the Api Connection Object
 --The displayname will be configured to be the same as the name of the connection
 Requires an authenticated session, either Az.Accounts or az cli
 "@
-    Alias       = "Arm.Set-Arm.Connections.ManagedApis.Servicebus.ConnectionString.AsArmObject"
+    Alias       = "Arm.Set-Arm.Connections.ManagedApis.LogAnalyticsDataCollector.AsArmObject"
 }
 
-Task -Name "Set-Arm.Connections.ManagedApis.Servicebus.ConnectionString.AsArmObject" @parm -Action {
+Task -Name "Set-Arm.Connections.ManagedApis.LogAnalyticsDataCollector.AsArmObject" @parm -Action {
     Set-TaskWorkDirectory
 
     # We can either use the az cli or the Az modules
     $tools = Get-PSFConfigValue -FullName PsLogicAppExtractor.Execution.Tools
         
     $found = $false
-    $conType = "servicebus"
+    $conType = "azureloganalyticsdatacollector"
 
     $armObj = Get-TaskWorkObject
 
     foreach ($connectionObj in $armObj.resources[0].properties.parameters.'$connections'.value.PsObject.Properties) {
-        if ($connectionObj.Value.id -like "*managedApis/servicebus*") {
-        
-            # This should only handle the classic Servicebus connections based on ListKey
-            if ($connectionObj.Value.connectionProperties.authentication.type -eq "ManagedServiceIdentity") { continue }
-        
+        if ($connectionObj.Value.id -like "*managedApis/azureloganalyticsdatacollector*" `
+                -and (-not ($connectionObj.Value.id -like "*[*(*)*]*"))) {
+            
             $found = $true
         
             # Fetch the details from the connection object
@@ -47,29 +44,30 @@ Task -Name "Set-Arm.Connections.ManagedApis.Servicebus.ConnectionString.AsArmObj
 
             # Fetch base template
             $pathArms = "$(Get-PSFConfigValue -FullName PsLogicAppExtractor.ModulePath.Base)\internal\arms"
-            $apiObj = Get-Content -Path "$pathArms\API.SB.ConnectionString.json" -Raw | ConvertFrom-Json
-
+            $apiObj = Get-Content -Path "$pathArms\API.LogAnalyticsDataCollector.InstrumentKey.json" -Raw | ConvertFrom-Json
+            
             # Set the names of the parameters
             $Prefix = Get-PSFConfigValue -FullName PsLogicAppExtractor.prefixsuffix.connection.prefix
-            $conStrPreSuf = Format-Name -Type "Connection" -Prefix $Prefix -Suffix "_ConnectionString" -Value "$($connectionObj.Name)"
+            $parmApicId = Format-Name -Type "Connection" -Prefix $Prefix -Value "$($connectionObj.Name)"
 
-            $idPreSuf = Format-Name -Type "Connection" -Value "$($connectionObj.Name)"
+            $parmApicInstrumentKey = Format-Name -Type "Connection" -Prefix $Prefix -Suffix "_InstrumentKey" -Value "$($connectionObj.Name)"
             
-            $armObj = Add-ArmParameter -InputObject $armObj -Name "$idPreSuf" `
+            $armObj = Add-ArmParameter -InputObject $armObj -Name "$parmApicId" `
                 -Type "string" `
                 -Value $conName `
                 -Description "The name / id of the ManagedApi connection object that is being utilized by the Logic App. Will be for the trigger and other actions that depend on connections."
             
-            $armObj = Add-ArmParameter -InputObject $armObj -Name "$conStrPreSuf" `
-                -Type "SecureString" `
-                -Value "Endpoint=sb://[ServicebusNamespace].servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=[AccessKeyValue]" `
-                -Description "The complete connection string for the namespace. Please note that you might need ';EntityPath=XYZ' appended based on your needs. ($($connectionObj.Name))"
-
+            $armObj = Add-ArmParameter -InputObject $armObj -Name "$parmApicInstrumentKey" `
+                -Type "string" `
+                -Value "$($resObj.Properties.parameterValues.username)" `
+                -Description "The InstrumentKey / Username as provided from Azure Log Analytics."
+                
             # Update the api object properties
-            $apiObj.Name = "[parameters('$idPreSuf')]"
-            $apiObj.properties.displayName = "[parameters('$idPreSuf')]"
-            $apiObj.properties.parameterValues.connectionString = $apiObj.properties.parameterValues.connectionString.Replace("##CONNECTIONSTRING##", "$conStrPreSuf")
+            $apiObj.Name = "[parameters('$parmApicId')]"
+            $apiObj.properties.displayName = "[parameters('$parmApicId')]"
             
+            $apiObj.properties.parameterValues.username = "[parameters('$parmApicInstrumentKey')]"
+
             # Append the new resource to the ARM template
             $armObj.resources += $apiObj
 
@@ -79,11 +77,11 @@ Task -Name "Set-Arm.Connections.ManagedApis.Servicebus.ConnectionString.AsArmObj
             }
 
             # Add the new resource to the dependsOn array, so that the deployment will work
-            $armObj.resources[0].dependsOn += "[resourceId('Microsoft.Web/connections', parameters('$idPreSuf'))]"
+            $armObj.resources[0].dependsOn += "[resourceId('Microsoft.Web/connections', parameters('$parmApicId'))]"
 
             # Adjust the connection object to depend on the same name
-            $connectionObj.Value.connectionId = "[resourceId('Microsoft.Web/connections', parameters('$idPreSuf'))]"
-            $connectionObj.Value.connectionName = "[parameters('$idPreSuf')]"
+            $connectionObj.Value.connectionId = "[resourceId('Microsoft.Web/connections', parameters('$parmApicId'))]"
+            $connectionObj.Value.connectionName = "[parameters('$parmApicId')]"
             $connectionObj.Value.id = "[format('/subscriptions/{0}/providers/Microsoft.Web/locations/{1}/managedApis/$conType', subscription().subscriptionId, parameters('logicAppLocation'))]"
         }
     }
